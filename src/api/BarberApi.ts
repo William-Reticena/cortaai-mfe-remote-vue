d379b9dc81b0be8d4d26f2f3942fd12aba4d94d5
William Reticena <william.wsd43@gmail.com>
2026-05-01 17:11:10 -0300

    fix: update API endpoints to include versioning and add authorization token handling

--- src/api/BarberApi.ts
+++ src/api/BarberApi.ts
@@ -5,18 +5,18 @@ import { CreateOfferServiceRequest, UpdateBarbershopDetailsRequest } from '@/sha
 
 export class BarbersApi {
   static async getBarbershopDetails(id: number) {
-    const response = await api.get<BarbershopDetailsResponse>(`/barbershops/${id}`);
+    const response = await api.get<BarbershopDetailsResponse>(`/v1/barbershops/${id}`);
 
     return new BarbershopDetailsResponse(response.data);
   }
 
   static async createOfferService(request: CreateOfferServiceRequest) {
-    const response = await api.post('/offers', request, { headers: { Authorization: 1 } });
+    const response = await api.post('/v1/offers', request);
     return response.data;
   }
 
   static async updateBarbershopDetails(request: UpdateBarbershopDetailsRequest) {
-    const response = await api.put<UpdateBarbershopDetailsResponse>('/barbershops', request, { headers: { Authorization: 1 } });
+    const response = await api.put<UpdateBarbershopDetailsResponse>('/v1/barbershops', request);
 
     return new UpdateBarbershopDetailsResponse(response.data);
   }
